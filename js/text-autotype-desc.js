389d4f77d8cfe6d8ceef4c0110f8025b14fc770f
Sunil Sapkota <suneelsapkota@gmail.com>
2024-06-28 18:20:30 +0545

    chore: Refactor autotype description code
    The autotype description code has been refactored to improve readability and maintainability. The variable names have been updated, unnecessary code has been removed, and the code has been formatted for consistency.

--- js/text-autotype-desc.js
+++ js/text-autotype-desc.js
@@ -1,52 +1,38 @@
 var i = 1;
-var j= -1;
-var list = ['Law Stutent'];
-var a=list[0];
+var j = -1;
+var list = ["Law Student"];
+var a = list[0];
 var len = list.length;
 var num = 0;
 
-function txt(){
-	var strs = a.slice(0,i);
+function txt() {
+  var strs = a.slice(0, i);
 
-	$("#textdisp").html(strs +'|');
+  $("#textdisp").html(strs + "|");
 
-	if(i >= 0)
-	{
-		if (i != a.length)
-			{
-				i++;
-			}
-		else
-			{
-				i = -1;}
-	}
-	else
-	{
-		if(-1*i != a.length)
-			{
-				i--;
-			}
-		else
-			{
-				i=1;
-				var flag = ret_index(a);
-				a = list[flag];
-			}
-
-
-	}
-
-
-
+  if (i >= 0) {
+    if (i != a.length) {
+      i++;
+    } else {
+      i = -1;
+    }
+  } else {
+    if (-1 * i != a.length) {
+      i--;
+    } else {
+      i = 1;
+      var flag = ret_index(a);
+      a = list[flag];
+    }
+  }
 }
-function ret_index(a){
-	var index = list.indexOf(a);
-	if (index!= (len-1))
-		{return (index+1);}
-	else
-		{return 0;}
-
+function ret_index(a) {
+  var index = list.indexOf(a);
+  if (index != len - 1) {
+    return index + 1;
+  } else {
+    return 0;
+  }
 }
 
-
-setInterval(txt,170);
+setInterval(txt, 170);
